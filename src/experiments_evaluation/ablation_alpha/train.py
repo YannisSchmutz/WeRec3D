76854896ec78b0d6ff633b04a43b603ee34f1c60
Yannis Schmutz <yannis.schmutz@bfh.ch>
2024-08-28 10:37:59 +0200

    Bugfix

--- src/experiments_evaluation/ablation_alpha/train.py
+++ src/experiments_evaluation/ablation_alpha/train.py
@@ -94,9 +94,8 @@ def main(exp_name, alpha, percentage, load_last):
             print(f"[*] Loading the weights of the last trained model ({load_last}%)")
             model.load_weights(f"{CHECKPOINT_FILE_PATH}p{load_last}/")
 
-    checkpoint_filepath = f"{CHECKPOINT_FILE_PATH}p{percentage}/"
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_filepath,
+        filepath=f"{CHECKPOINT_FILE_PATH}p{percentage}_alpha{alpha}.weights.h5",
         save_weights_only=True,
         monitor='val_loss',
         mode='min',
